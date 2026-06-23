caa00649a48eeb4235fc56c01d05c0bff23f1501
Muhindhar <muhindharsv27@gmail.com>
2026-06-23 16:50:47 +0530

    added the handson files

--- src/JavaCore/Voting.java
+++ src/JavaCore/Voting.java
@@ -1,5 +1,7 @@
 package JavaCore;
+
 import java.util.*;
+
 public class Voting {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -7,15 +9,13 @@ public class Voting {
 		int age = sc.nextInt();
 		System.out.println("Enter the weight : ");
 		int wt = sc.nextInt();
-		if(age>18) {
-			if(wt>50) {
+		if (age > 18) {
+			if (wt > 50) {
 				System.out.println("Eligible to donate blood");
-			}
-			else {
+			} else {
 				System.out.println("You are under weight and not eligible to donate blood");
 			}
-		}
-		else {
+		} else {
 			System.out.println("You are under age and not eligible to donate blood");
 		}
 	}
